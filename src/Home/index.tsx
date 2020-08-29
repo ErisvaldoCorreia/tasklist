ca5daf557a00cc99f5a0cd94e93dff99fa2abd38
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-08-29 00:06:45 -0300

    Create Cabeçalho

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, Titulo } from './styles';
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+        <StatusBar style='light' />
+        <Titulo>Minhas Tarefas</Titulo>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ccc',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Home/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+    flex: 1;
+    background: #171d31;
+`;
+
+export const Titulo = styled.Text`
+    margin-top: 30px;
+    padding-bottom: 20px;
+    font-size: 25px;
+    color: #fff;
+    text-align: center;
+`;
