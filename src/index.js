f49c44e44d632e5b0badc0f96ba70c6eea158b2d
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-09-01 20:40:59 -0300

    Componentizando Telas

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Titulo } from './styles';
+
+export default function Home() {
+  return (
+    <Container>
+      <Titulo>TaskList</Titulo>
+    </Container>
+  );
+}
