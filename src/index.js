ddc739ca25406f0ff8fa5e21c7843aee35d992b1
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-09-01 20:51:43 -0300

    botão add estilo fab

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Container, Titulo } from './styles';
+import { Container, Titulo, BotaoAdd } from './styles';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Home() {
   return (
     <Container>
+      
       <Titulo>TaskList</Titulo>
+
+      <BotaoAdd>
+          <Ionicons name="ios-add" size={35} color="#fff" />
+      </BotaoAdd>
+      
     </Container>
   );
 }
--- src/styles.js
+++ src/styles.js
@@ -12,3 +12,16 @@ export const Titulo = styled.Text`
     color: #fff;
     text-align: center;
 `;
+
+export const BotaoAdd = styled.TouchableOpacity`
+    position: absolute;
+    width: 60px;
+    height: 60px;
+    border-radius: 30px;
+    background: #0094ff;
+    justify-content: center;
+    align-items: center;
+    right: 25px;
+    bottom: 25px;
+    z-index: 9;
+`;
