618d1094cfc2f184afffe4379ed60968f536200a
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-09-01 21:02:54 -0300

    TaskList Flat base

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container, Titulo, BotaoAdd } from './styles';
 import { Ionicons } from '@expo/vector-icons';
 
 export default function Home() {
+
+    const [task, setTask] = useState([
+        { key: 1, task: 'Tarefa Teste'},
+        { key: 2, task: 'Tarefa Teste'},
+        { key: 3, task: 'Tarefa Teste'},
+    ]);
+
   return (
     <Container>
       
       <Titulo>TaskList</Titulo>
 
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        data={task}
+        keyExtractor={ (item) => String(item.key) }
+        renderItem={ ({item}) => <TaskList data={item} /> }
+      />
+
       <BotaoAdd>
           <Ionicons name="ios-add" size={35} color="#fff" />
       </BotaoAdd>
-      
+
     </Container>
   );
 }
